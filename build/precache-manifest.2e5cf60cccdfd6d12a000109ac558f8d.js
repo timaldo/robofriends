295d2683a813ab04a53daade63a65e7296eaf1da
timaldo <info@timdorsett.com>
2020-03-18 13:04:25 +0000

    initial upload of files

---
+++ build/precache-manifest.2e5cf60cccdfd6d12a000109ac558f8d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25eb74b495ccaf52f36234a1ffdba5b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36aeec622dcf43330225",
+    "url": "/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "90d7041ee6a36414282c",
+    "url": "/static/css/main.938d4baa.chunk.css"
+  },
+  {
+    "revision": "36aeec622dcf43330225",
+    "url": "/static/js/2.1f0aded6.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.1f0aded6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90d7041ee6a36414282c",
+    "url": "/static/js/main.136f01bd.chunk.js"
+  },
+  {
+    "revision": "5bba5901d09af5808707",
+    "url": "/static/js/runtime-main.e2cc1168.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
