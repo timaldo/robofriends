d5bb725622a3a3a8f7357c6f4578c724d007ea93
timaldo <info@timdorsett.com>
2020-05-22 15:53:24 +0100

    added search label

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,15 +1,20 @@
-import React from 'react';
-
+import React from "react";
 
 const SearchBox = ({ searchfield, searchChange }) => {
   return (
-    <div className='pa2'>
-      <input className='pa3 ba b--green bg-lightest-blue'
-        type='search'
-        placeholder='Search Robots'
-        onChange={ searchChange } />
+    <div className="pa2">
+      <label>
+        {"Robot Search"}
+
+        <input
+          className="pa3 ba b--green bg-lightest-blue"
+          type="search"
+          placeholder="Search Robots"
+          onChange={searchChange}
+        />
+      </label>
     </div>
   );
-}
+};
 
 export default SearchBox;
